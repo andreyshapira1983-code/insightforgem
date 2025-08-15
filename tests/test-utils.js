196f4a10a122ecf3b73686716ed88d00e36849c8
andreyshapira1983-code <andreyshapira1983@gmail.com>
2025-08-15 21:43:43 +0300

    delet

--- tests/test-utils.js
+++
@@ -1,9 +0,0 @@
-export const run = async (name, fn) => {
-  try {
-    await fn();
-    console.log('\u2713', name);
-  } catch (e) {
-    console.error('\u2717', name, e);
-    process.exitCode = 1;
-  }
-};
